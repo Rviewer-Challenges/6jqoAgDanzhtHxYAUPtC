6c3067da0c9aeb3b7417d8a835489f317d2ea354
Darío Gallegos <05dariogallegos@gmail.com>
2022-10-12 23:09:22 +0200

    Basic struct  Xcode project

---
+++ Exchages/Exchages/ExchagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchagesApp.swift
+//  Exchages
+//
+//  Created by Dario Gallegos on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ExchagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
