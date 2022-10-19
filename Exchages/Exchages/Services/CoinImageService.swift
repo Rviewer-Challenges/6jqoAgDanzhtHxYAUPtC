7518a847323298325dccef19c0ac18f86420c9b1
Darío Gallegos <05dariogallegos@gmail.com>
2022-10-19 23:14:07 +0200

    Load image from cache or download if is necessary

--- Exchages/Exchages/Services/CoinImageService.swift
+++ Exchages/Exchages/Services/CoinImageService.swift
@@ -13,13 +13,26 @@ class CoinImageService {
     @Published var image: UIImage?
     private var imageSubsciption: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let loadedImage = fileManager.loadImage(imageName: imageName, folderName: folderName) {
+            image =  loadedImage
+        }
+        else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         
         guard let url = URL(string: coin.image) else {
             return
@@ -30,8 +43,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubsciption?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubsciption?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
